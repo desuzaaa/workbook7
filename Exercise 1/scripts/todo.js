0b11bdf1c4d1eb953471753e7c75f831695b961b
Kameron de Suza <z6nekam@gmail.com>
2024-11-19 16:01:09 -0500

    progressed on js and html

--- Exercise 1/scripts/todo.js
+++ Exercise 1/scripts/todo.js
@@ -1,2 +1,19 @@
-"use strict"
+"use strict";
+
+let idInput = document.querySelector("#idInput");
+let detailsDiv = document.querySelector("#detailsDiv");
+
+async function getToDo(id) {
+  let promise = fetch("https://jsonplaceholder.typicode.com/todos/1" + id);
+  let response = await promise;
+  let todo = await response.json();
+  detailsDiv.innerText = `Your title is ${todo.title}`
+}
+
+getToDo();
+
+function showDetails() {
+  let id = idInput.value;
+  getToDo(id);
+}
 
