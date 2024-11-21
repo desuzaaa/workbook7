5149f65d02e382b367b8cba5acf38fa631707c95
Kameron de Suza <z6nekam@gmail.com>
2024-11-21 15:22:33 -0500

    still gotta work on details and newcourse

--- courses-exercise/scripts/courses.js
+++ courses-exercise/scripts/courses.js
@@ -3,12 +3,10 @@
 let url = "http://localhost:8081/api/courses/";
 
 let coursesTBody = document.querySelector("#coursesTBody");
-
 let coursesList = document.querySelector("#coursesList");
 
 async function loadCourses() {
   let response = await fetch(url);
-
   let courses = await response.json();
 
   displayTable(courses);
@@ -40,46 +38,53 @@ function displayTable(courses) {
     td5.innerText = course.startDate;
     tr.appendChild(td5);
 
+    let td6 = document.createElement("td");
+    let anchor = document.createElement("a");
+    anchor.href = `http://localhost:8081/details.html?courseid=${course.id}`;
+    anchor.textContent = "See details";
+    td6.appendChild(anchor);
+    tr.appendChild(td6);
+
     coursesTBody.appendChild(tr);
   }
 }
 
 function displayCards(courses) {
   for (const course of courses) {
-    createCourseCard(course.courseName, course.courseNum, course.dept, course.startDate, course.instructor);
+    createCourseCard(course.courseName, course.courseNum, course.dept, course.startDate, course.instructor, course.id);
   }
 }
 
-function createCourseCard(courseName, courseNumber, department, startDate, instructor) {
-  
+function createCourseCard(courseName, courseNumber, department, startDate, instructor, courseId) {
   const cardContainer = document.createElement("div");
   cardContainer.className = "card";
   cardContainer.style.width = "18rem";
 
-  
   const cardBody = document.createElement("div");
   cardBody.className = "card-body";
-
 
   const cardTitle = document.createElement("h5");
   cardTitle.className = "card-title";
   cardTitle.textContent = courseName;
 
-  
   const cardSubtitle = document.createElement("h6");
   cardSubtitle.className = "card-subtitle mb-2 text-body-secondary";
   cardSubtitle.textContent = `${courseNumber}, ${department}`;
- 
+
   const cardText = document.createElement("p");
   cardText.className = "card-text";
   cardText.textContent = `Starts: ${startDate}, Instructor: ${instructor}`;
 
-  
+  const cardLink = document.createElement("a");
+  cardLink.href = `http://localhost:8081/details.html?courseid=${courseId}`;
+  cardLink.textContent = "See details";
+  cardLink.className = "btn btn-primary mt-2";
+
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(cardSubtitle);
   cardBody.appendChild(cardText);
+  cardBody.appendChild(cardLink);
   cardContainer.appendChild(cardBody);
-
 
   coursesList.appendChild(cardContainer);
 }
